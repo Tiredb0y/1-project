21a614cf2f26dd4f6ebe48b993e1f2b154d5f94f
8rs1 <arssani.info@gmail.com>
2023-12-14 16:28:56 +0330

    redesign footer

--- vue-project/src/components/footervue.vue
+++ vue-project/src/components/footervue.vue
@@ -5,62 +5,64 @@
       <footer>
         <div class="footer-container">
           <div class="contact-info">
-            <h3><RouterLink to="/contact" class="link">Contact Us</RouterLink></h3>
-          <p><RouterLink to="/" class="link">Home</RouterLink></p>
-          <p><RouterLink to="/about" class="link">About</RouterLink></p>
-          
+            <div class="footer-title">Quick Access Pages</div>
+          <div class="link-container"><RouterLink to="/" class="link">Home</RouterLink></div>
+          <div class="link-container"><RouterLink to="/about" class="link">About</RouterLink></div>
+          <div class="link-container"><RouterLink to="/contact" class="link">Contact Us</RouterLink></div>
           </div>
           <div class="social-media">
-            <h3>Follow Us</h3>
+            <div class="footer-title">Follow Us</div>
+            <div class="link-container"><a href="javascript:void(0)">Instagram</a></div>
+            <div class="link-container"><a href="javascript:void(0)">X (Twitter)</a></div>
+            <div class="link-container"><a href="javascript:void(0)">Facebook</a></div>
+            <div class="link-container"><a href="javascript:void(0)">Telegram</a></div>
           </div>
         </div>
-        <div class="copyright">
-          <p>Hello world</p>
-        </div>
+        <div class="copyright"><p>&copy; All right reserved for <a href="https://www.github.com/tiredb0y">tiredb0y GitHub</a></p></div>
       </footer>
 </template>
 <style scoped>
-.link{
+a {
+  color: rgb(11, 221, 211);
   text-decoration: none;
-  color: #fff;
+  letter-spacing: .05rem;
 }
-*{
-    display: flexbox;
+a:active {
+  color: darkcyan;
 }
-body {
+.link-container {
+  margin-top: 6px;
+}
+.copyright {
+  margin: 3px 0 0 0;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-size: .8rem;
+  letter-spacing: .09rem;
+  padding-left: 10px;
+}
+.copyright>p {
   margin: 0;
-  padding: 0;
-  font-family: Arial, sans-serif;
 }
-
 footer {
   background-color: #333;
   color: #fff;
-  padding: 20px 0;
+  padding: 10px 0 3px 0;
 }
 
 .footer-container {
-  display: flex;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
 }
 
 .contact-info{
     margin-left: 10px;
     
 }
-.social-media {
-  display: flex;
-  justify-content: center;
-  margin-right: 48%;
+
+.footer-title {
+  font-size: 1.5rem;
+  color: #f0f0f0;
 }
 
-h3 {
-  color: #fff;
-}
-
-.copyright {
-  margin-top: 20px;
-  text-align: center;
-}
 
 </style>
